870af70277bba14233de91ef27f9e8e2603b3cb1
Nikita0x <klirmio21@gmail.com>
2025-04-19 01:00:39 +0300

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import BaseTable from "@/components/table";
 import Modal from "@/components/modal";
+import BaseTable from "@/components/table";
 
 export default function Home() {
 	return (
